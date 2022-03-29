5f28b4661b2fd4a8f4210faa299145874ab78e21
PJPalma <pt_palma@hotmail.com>
2022-03-29 23:02:04 +0100

    end this

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,7 +1,7 @@
 // Iteration #1: Find the maximum
 function maxOfTwoNumbers() {}
 
-
+//biggychin
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
